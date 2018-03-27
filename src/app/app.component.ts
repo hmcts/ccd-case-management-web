d0102701cf27f801aafee0470fbeb22d1d2a7f4c
Valentin Laurin <valentin.laurin@digi2al.co.uk>
2018-03-27 12:39:56 +0100

    Open sourcing RDM-1542

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 import '../style/app.scss';
 
 @Component({
   selector: 'ccd-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {}

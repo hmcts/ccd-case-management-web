8ad57aefafffdb0fd6152c9483d34382269bbe30
andrewf <andrzej.folga@hmcts.net>
2018-04-02 20:08:01 +0100

    RDM-1925 Build values of complex field's nested fields.

--- src/app/shared/palette/complex/read-complex-field.component.ts
+++ src/app/shared/palette/complex/read-complex-field.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 import { AbstractFieldReadComponent } from '../base-field/abstract-field-read.component';
+import { CaseField } from '../../domain/definition/case-field.model';
 
 @Component({
   selector: 'ccd-read-complex-type-field',
   templateUrl: './read-complex-field.html',
   styleUrls: ['./read-complex-field.scss']
 })
-export class ReadComplexFieldComponent extends AbstractFieldReadComponent {}
+export class ReadComplexFieldComponent extends AbstractFieldReadComponent {
+
+  getComplexFields() {
+    let complexFields = [];
+    this.caseField.field_type.complex_fields.forEach((elem, index) => {
+      let elemCopy = Object.assign({}, elem);
+      elemCopy.value = this.caseField.value[elem.id];
+      if (!elem.value) {
+        complexFields[index] = elemCopy;
+      }
+    });
+    return complexFields;
+  }
+}

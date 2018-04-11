0b6f26e4bfcffde7c434d7eff418161c88811abe
Anthony Gilks <>
2018-04-11 12:50:05 +0100

    changes-to-docker-configuration

--- test/script/run-e2e-tests.sh
+++ test/script/run-e2e-tests.sh
@@ -15,17 +15,20 @@ Xvfb :10 -screen 0 1920x1080x24 2>&1 >/dev/null &
 #echo "Starting webdriver"
 #node ./node_modules/protractor/bin/webdriver-manager start [OR webdriver-manager start] &
 #echo "Finished starting webdriver"
-sleep 20
 
 echo "Running Protractor tests"
 # The 'uluwatu-e2e-protractor' test project launch configuration file (e2e.conf.js) should be passed here.
-cd ..
+
+cd project/ccd-case-management-web
+
+echo "Starting CCD smoke tests"
 DISPLAY=:10 yarn $@
 export RESULT=$?
 
 echo "Protractor tests have done"
 # Close the XVFB display
 killall Xvfb
+
 # Remove temporary folders
 rm -rf .config .local .pki .cache .dbus .gconf .mozilla
 # Set the file access permissions (read, write and access) recursively for the result folders

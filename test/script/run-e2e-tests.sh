1978c7d4457b7e6568b62d0a73c637e9fcc3843a
agilks <32171912+agilks@users.noreply.github.com>
2018-04-11 15:49:02 +0100

    Update run-e2e-tests.sh

--- test/script/run-e2e-tests.sh
+++ test/script/run-e2e-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+
 # Move to the Protractor test project folder
 cd $HOME
 
-# Remove previous Allure results
-rm -rf allure-results
+cd ccd-case-management-web
 
 # X11 for Ubuntu is not configured! The following configurations are needed for XVFB.
 # Make a new display :21 with virtual screen 0 with resolution 1024x768 24dpi
@@ -18,20 +18,11 @@ Xvfb :10 -screen 0 1920x1080x24 2>&1 >/dev/null &
 
 echo "Running Protractor tests"
 # The 'uluwatu-e2e-protractor' test project launch configuration file (e2e.conf.js) should be passed here.
-
-cd project/ccd-case-management-web
-
-echo "Starting CCD smoke tests"
 DISPLAY=:10 yarn $@
 export RESULT=$?
 
 echo "Protractor tests have done"
 # Close the XVFB display
 killall Xvfb
-
-# Remove temporary folders
-rm -rf .config .local .pki .cache .dbus .gconf .mozilla
-# Set the file access permissions (read, write and access) recursively for the result folders
-chmod -Rf 777 allure-results test-results
 
 exit $RESULT

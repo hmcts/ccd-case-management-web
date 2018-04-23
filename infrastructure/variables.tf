575ee4596a6d3e222b4ef4d4dd689f05fcbcd4df
Valentin Laurin <me@vlaurin.eu>
2018-04-23 11:16:42 +0100

    Add support for Sandbox DM CNP

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -38,7 +38,7 @@ variable "ccd_gateway_url" {
 }
 
 variable "document_management_url" {
-  default = "https://api-gateway.test.dm.reform.hmcts.net"
+  default = ""
 }
 
 variable "ccd_print_service_url" {

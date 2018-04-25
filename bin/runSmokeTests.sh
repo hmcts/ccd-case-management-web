401c38b71528572ef30180c7e3d889900b88f26a
Hemanth Potipati <hrpotipati@gmail.com>
2018-04-25 16:30:39 +0100

    Update runSmokeTests.sh

--- bin/runSmokeTests.sh
+++ bin/runSmokeTests.sh
@@ -1,3 +1,5 @@
-cd test
+#!/bin/bash
+
 docker build -t ccd-protractor:latest test/. 
+
 docker run -t --rm -e CCD_CASEWORKER_AUTOTEST_EMAIL=$CCD_CASEWORKER_AUTOTEST_EMAIL -e CCD_CASEWORKER_AUTOTEST_PASSWORD=$CCD_CASEWORKER_AUTOTEST_PASSWORD -e TEST_FRONTEND_URL=$TEST_FRONTEND_URL --name protractor-runner ccd-protractor:latest test:smokeDocker

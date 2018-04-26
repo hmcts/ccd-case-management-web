c023a49b88d212147968180403538409ece557b0
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-04-26 15:52:36 +0100

    last fixes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,7 +30,6 @@ module "case-management-web" {
     CCD_GW_LOGOUT_URL = "${local.ccd_gateway_url}/logout"
     CCD_API_URL = "${local.ccd_gateway_url}/aggregated"
     CCD_DATA_URL = "${local.ccd_gateway_url}/data"
-    CCD_ACTIVITY_URL = "" // Activity disabled until it's deployed on CNP
     CCD_GW_OAUTH2_URL = "${local.ccd_gateway_url}/oauth2"
     CCD_GW_OAUTH2_CLIENT_ID = "ccd_gateway"
     DM_URL = "${local.ccd_gateway_url}/documents"
@@ -40,6 +39,11 @@ module "case-management-web" {
     PRINT_SERVICE_URL = "${local.ccd_gateway_url}/print"
     PRINT_SERVICE_URL_REMOTE = "${local.ccd_print_service_url}"
     WEBSITE_NODE_DEFAULT_VERSION = "8.9.4"
+    CCD_ACTIVITY_URL = "" // Activity disabled until it's deployed on CNP
+    CCD_ACTIVITY_NEXT_POLL_REQUEST_MS = 5000;
+    CCD_ACTIVITY_RETRY = 5;
+    CCD_ACTIVITY_BATCH_COLLECTION_DELAY_MS = 1;
+    CCD_ACTIVITY_MAX_REQUEST_PER_BATCH = 25; // Better have this same as CCD_PAGE_SIZE;
   }
 }
 

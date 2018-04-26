5ee3c2ca0792e17aad774fcdb8d5334a549f4fac
Anthony Gilks <>
2018-04-26 17:35:22 +0100

    set-outputs-for-vault

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,3 +1,11 @@
 output "case_management_web_deployment_endpoint" {
   value = "${module.case-management-web.gitendpoint}"
 }
+
+output "vaultUri" {
+  value = "${module.ccd-case-management-web-vault.key_vault_uri}"
+}
+
+output "vaultName" {
+  value = "${module.ccd-case-management-web-vault.key_vault_name}"
+}

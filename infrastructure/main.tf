463fea0bc4beb446a944dbc7c52b4877be08b90a
Adrian Cho <waihung.cho@hmcts.net>
2018-05-15 11:39:25 +0100

    Link preview gateway to aat

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,7 +6,8 @@ locals {
   env_ase_url = "${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
 
   default_ccd_gateway_url = "https://ccd-api-gateway-web-${local.env_ase_url}"
-  ccd_gateway_url = "${var.ccd_gateway_url != "" ? var.ccd_gateway_url : local.default_ccd_gateway_url}"
+  non_preview_ccd_gateway_url = "${var.ccd_gateway_url != "" ? var.ccd_gateway_url : local.default_ccd_gateway_url}"
+  ccd_gateway_url = "${var.env == "preview" ? "http://ccd-api-gateway-web-aat.service.core-compute-aat.internal" : local.non_preview_ccd_gateway_url}"
 
   default_ccd_print_service_url = "https://ccd-case-print-service-${local.env_ase_url}"
   ccd_print_service_url = "${var.ccd_print_service_url != "" ? var.ccd_print_service_url : local.default_ccd_print_service_url}"

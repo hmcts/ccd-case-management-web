1ff45580f2b0cf7b4e376ed7adc77107b80f6d4f
Karamath <karamath.mohamed@hmcts.net>
2018-05-24 16:53:24 +0100

    RDM-1914: Improved the import of material CSS styling package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import '../style/app.scss';
+import '../../node_modules/@angular/material/prebuilt-themes/indigo-pink.css';
 
 @Component({
   selector: 'ccd-app',

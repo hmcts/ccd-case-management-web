9592045013370720f657263c3a7e98155d6c1a64
Karamath <karamath.mohamed@hmcts.net>
2018-05-25 10:27:25 +0100

    Revert "RDM-1914: Improved the import of material CSS styling package"
    This reverts commit 1ff45580f2b0cf7b4e376ed7adc77107b80f6d4f.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import '../style/app.scss';
-import '../../node_modules/@angular/material/prebuilt-themes/indigo-pink.css';
 
 @Component({
   selector: 'ccd-app',

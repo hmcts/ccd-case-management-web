5eeee07dd5df3dec7ba7aba5cf265b082d89cb06
Hemanth Potipati <hrpotipati@gmail.com>
2018-05-29 15:39:54 +0100

    Revert "RDM-2185 - Redirect back to login page on logout"

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -21,6 +21,10 @@ export class AuthService {
     this.document.location.href = `${loginUrl}?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
   }
 
+  public signOut(): void {
+    this.document.location.href = this.appConfig.getLogoutUrl();
+  }
+
   public redirectUri(): string {
     return this.document.location.origin + AuthService.PATH_OAUTH2_REDIRECT;
   }

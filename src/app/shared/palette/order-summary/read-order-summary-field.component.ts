66c25180991fc75a77922628974100e5b6ecf33a
andrewf <andrzej.folga@hmcts.net>
2018-05-31 12:13:42 +0100

    RDM-2243 Remove compilation errors.

--- src/app/shared/palette/order-summary/read-order-summary-field.component.ts
+++ src/app/shared/palette/order-summary/read-order-summary-field.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { AbstractFieldReadComponent } from '../base-field/abstract-field-read.component';
-import { Fee } from './fee.model';
-import { OrderSummary } from './order-summary.model';
 
 @Component({
   selector: 'ccd-read-order-summary-field',

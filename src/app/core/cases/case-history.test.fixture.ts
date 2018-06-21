3936c9fed36cc32855959ee9f84884905afac850
gbenadikar <giri.benadikar@hmcts.net>
2018-06-21 13:48:41 +0100

    RDM-597 - review comments

--- src/app/core/cases/case-history.test.fixture.ts
+++ src/app/core/cases/case-history.test.fixture.ts
@@ -1,19 +1,19 @@
-import { CaseHistory } from './case-history.model';
+import { CaseHistory, CaseHistoryCaseType } from './case-history.model';
 import { createCaseViewEvent } from './case-view-event.test.fixture';
 import { createCaseTabArray } from './case-tab.test.fixture';
+import { createJurisdiction } from '../../shared/domain/definition/jurisdiction.test.fixture';
 
 export let createCaseHistory = () => {
   const caseHistory = new CaseHistory();
+  const caseHistoryCaseType = new CaseHistoryCaseType();
 
   caseHistory.case_id = '1';
-  caseHistory.case_type = {
-    id: 'TestAddressBookCase',
-    name: 'Test Address Book Case',
-    jurisdiction: {
-      id: 'TEST',
-      name: 'Test',
-    }
-  };
+
+  caseHistoryCaseType.id = 'TestAddressBookCase';
+  caseHistoryCaseType.name = 'Test Address Book Case';
+  caseHistoryCaseType.jurisdiction = createJurisdiction();
+  caseHistory.caseType = caseHistoryCaseType;
+
   caseHistory.tabs = createCaseTabArray();
   caseHistory.event = createCaseViewEvent();
 

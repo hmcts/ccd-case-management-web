e89a5c70e8b343d3957c0f66a891be058576886e
Karamath <karamath.mohamed@hmcts.net>
2018-08-09 11:49:20 +0100

    RDM-2253: Changed require to import

--- server.ts
+++ server.ts
@@ -2,12 +2,13 @@ import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 import { enableProdMode } from '@angular/core';
 // Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
+import { ngExpressEngine } from '@nguniversal/express-engine';
 // Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
 import { join } from 'path';
+import { xFrameOptions } from 'x-frame-options';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -55,7 +56,6 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
-let xFrameOptions = require('x-frame-options');
 app.use(xFrameOptions());
 
 app.get('/health', (req, res) => {

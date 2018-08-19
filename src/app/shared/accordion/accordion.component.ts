617c128e0dcb3d10860ec628138773451c69480e
Karamath <karamath.mohamed@hmcts.net>
2018-08-19 01:08:37 +0100

    RDM-2542: Accordion component added

---
+++ src/app/shared/accordion/accordion.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ccd-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.scss']
+})
+export class AccordionComponent implements OnInit {
+
+  @Input() title: string;
+  isHidden = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getImage() {
+    if (this.isHidden) {
+      return 'img/accordion-plus.png';
+    } else {
+      return 'img/accordion-minus.png';
+    }
+  }
+}

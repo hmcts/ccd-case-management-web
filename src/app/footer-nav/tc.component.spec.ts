cd4d79d1f7ea11291e060a3639a73ab2fce4a517
Karamath <karamath.mohamed@hmcts.net>
2018-08-20 11:01:44 +0100

    RDM-2542: Privacy policy page - work in progress

--- src/app/footer-nav/tc.component.spec.ts
+++ src/app/footer-nav/tc.component.spec.ts
@@ -1,14 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TcComponent } from './tc.component';
+import { Location } from '@angular/common';
+import { MockComponent } from 'ng2-mock-component';
 
 describe('TcComponent', () => {
+
+  let AccordionComponent: any = MockComponent({ selector: 'ccd-accordion', inputs: [
+      'title'
+    ]});
+
   let component: TcComponent;
   let fixture: ComponentFixture<TcComponent>;
+  let location: Location;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TcComponent ]
+      declarations: [
+        TcComponent,
+        AccordionComponent,
+      ],
+      providers: [
+        { provide: Location, useValue: location }
+      ]
     })
     .compileComponents();
   }));

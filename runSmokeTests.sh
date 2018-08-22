02764dbddf7ff718e42eab805b1677934dab169e
Valentin Laurin <me@vlaurin.eu>
2018-08-22 11:19:49 +0100

    RDM-1747: Smoke tests with Docker Compose
    Make smoke tests use Docker Compose instead of simple Docker

--- runSmokeTests.sh
+++ runSmokeTests.sh
@@ -1,3 +1,4 @@
-cd test
-docker build -t ccd-protractor:latest .
-docker run -it --rm -e CCD_CASEWORKER_AUTOTEST_EMAIL=$CCD_CASEWORKER_AUTOTEST_EMAIL -e CCD_CASEWORKER_AUTOTEST_PASSWORD=$CCD_CASEWORKER_AUTOTEST_PASSWORD -e TEST_FRONTEND_URL=$TEST_FRONTEND_URL --name protractor-runner -v $(PWD):/protractor/project ccd-protractor:latest test:smokeDocker
+#!/bin/bash
+set -ex
+
+docker-compose -f docker-compose.tests.yml run --rm -u `id -u $USER` protractor test:smokeTests

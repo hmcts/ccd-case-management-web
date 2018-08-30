c1169e83682a14850c0aac721fca2ffc26d48f1f
Valentin Laurin <me@vlaurin.eu>
2018-08-30 14:32:17 +0100

    Fix: Remove docker container from Jenkins

--- bin/runSmokeTests.sh
+++ bin/runSmokeTests.sh
@@ -3,6 +3,6 @@ set -ex
 
 docker ps -a
 
-docker-compose -f docker-compose.tests.yml stop protractor
+docker-compose -f docker-compose.tests.yml down protractor
 
 docker-compose -f docker-compose.tests.yml run --name "ccd-case-management-protractor" --rm -u `id -u $USER` protractor test:smokeTests

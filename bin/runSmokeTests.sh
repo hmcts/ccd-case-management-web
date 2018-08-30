b238d22a8c36c43f82517c36924ece7337b09b6d
Valentin Laurin <me@vlaurin.eu>
2018-08-30 13:03:44 +0100

    Fix: Docker compose stop

--- bin/runSmokeTests.sh
+++ bin/runSmokeTests.sh
@@ -3,6 +3,6 @@ set -ex
 
 docker ps
 
-docker stop $(docker ps -f name=ccd-case-management-protractor --quiet)
+docker-compose -f docker-compose.tests.yml stop protractor
 
 docker-compose -f docker-compose.tests.yml run --name "ccd-case-management-protractor" --rm -u `id -u $USER` protractor test:smokeTests

3d4b6329e8b6e4ae465ad06e8fd108722bddb431
Valentin Laurin <me@vlaurin.eu>
2018-08-30 13:01:31 +0100

    Fix: List all running Docker containers

--- bin/runSmokeTests.sh
+++ bin/runSmokeTests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+docker ps
+
 docker stop $(docker ps -f name=ccd-case-management-protractor --quiet)
 
 docker-compose -f docker-compose.tests.yml run --name "ccd-case-management-protractor" --rm -u `id -u $USER` protractor test:smokeTests

35dca017cef1d5ff30dd88ba6e93f5334a60311c
Valentin Laurin <me@vlaurin.eu>
2018-08-30 12:40:12 +0100

    Fix: Stop running smoke test container

--- bin/runSmokeTests.sh
+++ bin/runSmokeTests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -ex
 
+docker stop $(docker ps -f name=ccd-case-management-protractor --quiet)
+
 docker-compose -f docker-compose.tests.yml run --name "ccd-case-management-protractor" --rm -u `id -u $USER` protractor test:smokeTests

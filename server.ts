ed517e086d09ea3623fcb92d21abc08b368e1d6c
Hemanth Potipati <hrpotipati@gmail.com>
2018-09-05 15:09:29 +0100

    add helmetjs

--- server.ts
+++ server.ts
@@ -15,6 +15,7 @@ enableProdMode();
 
 // Express server
 const app = express();
+const helmet = require('helmet');
 
 const PORT = process.env.PORT || 3451;
 const DIST_FOLDER = join(process.cwd());
@@ -50,6 +51,48 @@ const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/mai
 
 import { AppConfig } from './src/app/app.config';
 import { AppServerConfig } from './src/app/app.server.config';
+
+app.enable('trust proxy');
+
+// Security library helmet to verify 11 smaller middleware functions
+app.use(helmet());
+
+// Content security policy to allow just assets from same domain
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ['\'self\''],
+        fontSrc: ['\'self\' data:'],
+        scriptSrc: [
+            '\'self\'',
+            '\'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU=\'',
+            '\'sha256-AaA9Rn5LTFZ5vKyp3xOfFcP4YbyOjvWn2up8IKHVAKk=\'',
+            '\'sha256-G29/qSW/JHHANtFhlrZVDZW1HOkCDRc78ggbqwwIJ2g=\'',
+            'www.google-analytics.com',
+            `'nonce-${uuid}'`
+        ],
+        connectSrc: ['\'self\''],
+        mediaSrc: ['\'self\''],
+        frameSrc: ['\'none\''],
+        imgSrc: ['\'self\'', 'www.google-analytics.com'],
+        frameAncestors: ['\'self\'']
+    },
+    browserSniff: true,
+    setAllHeaders: true
+}));
+// Http public key pinning
+app.use(helmet.hpkp({
+    maxAge: 900,
+    sha256s: ['AbCdEf123=', 'XyzABC123=']
+}));
+
+// Referrer policy for helmet
+app.use(helmet.referrerPolicy({
+    policy: 'origin'
+}));
+
+app.use(helmet.noCache());
+
+app.use(helmet.xssFilter({setOnOldIE: true}));
 
 app.engine('html', ngExpressEngine({
   bootstrap: AppServerModuleNgFactory,

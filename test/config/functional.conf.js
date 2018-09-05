11c41275c288bcaffb51c9a93b83f82e551c5867
Hemanth Potipati <hrpotipati@gmail.com>
2018-09-05 11:02:34 +0100

    Create functional.conf.js

---
+++ test/config/functional.conf.js
@@ -0,0 +1,54 @@
+exports.config = {
+
+  /*
+     - before execution set the following environment variables:
+                CCD_CASEWORKER_AUTOTEST_EMAIL
+                CCD_CASEWORKER_AUTOTEST_PASSWORD
+                TEST_FRONTEND_URL
+     - to stop parallel execution: comment out shardTestFiles and maxInstances
+     - to stop tests running headless: comment out moz:firefoxOptions section
+  */
+
+  specs: [
+    './functional/specs/**/*.spec.js'
+  ],
+  exclude: [],
+
+  testFrontEndURL: process.env.TEST_URL || 'http://localhost:3451',
+
+  allScriptsTimeout: 110000,
+  restartBrowserBetweenTests: true,
+  untrackOutstandingTimeouts: true,
+
+  framework: 'jasmine2',
+
+  jasmineNodeOpts: {
+    showTiming: true,
+    showColors: true,
+    isVerbose: false,
+    includeStackTrace: false,
+    defaultTimeoutInterval: 400000
+  },
+  directConnect: true,
+
+  capabilities: {
+    'browserName': 'firefox'
+  },
+
+  /**
+   * Angular 2 configuration
+   *
+   * useAllAngular2AppRoots: tells Protractor to wait for any angular2 apps on the page instead of just the one matching
+   * `rootEl`
+   *
+   */
+  useAllAngular2AppRoots: true,
+
+  onPrepare: function () {
+    var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+    // add jasmine spec reporter
+    jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
+
+  },
+
+};

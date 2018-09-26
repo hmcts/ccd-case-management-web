376627fac8708629036e3c8f281da205c3bc01cd
Hemanth Potipati <hrpotipati@gmail.com>
2018-09-26 10:24:17 +0100

    Update runSmokeTests.sh

--- bin/runSmokeTests.sh
+++ bin/runSmokeTests.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-set -ex
+##set -ex
+##docker-compose -f docker-compose.tests.yml run --name "ccd-case-management-protractor" --rm -u `id -u $USER` protractor test:smokeTests
 
-docker-compose -f docker-compose.tests.yml run --name "ccd-case-management-protractor" --rm -u `id -u $USER` protractor test:smokeTests
+if [ -z "$CCD_CASEWORKER_AUTOTEST_EMAIL" ] ; 
+then echo "CCD_CASEWORKER_AUTOTEST_EMAIL null"; 
+else echo "CCD_CASEWORKER_AUTOTEST_EMAIL $CCD_CASEWORKER_AUTOTEST_EMAIL";
+fi;
+
+if [ -z "$CCD_CASEWORKER_AUTOTEST_PASSWORD" ]; 
+then echo "CCD_CASEWORKER_AUTOTEST_PASSWORD null"; 
+else echo "CCD_CASEWORKER_AUTOTEST_PASSWORD $CCD_CASEWORKER_AUTOTEST_PASSWORD";
+fi;
+
+if [ -z "$TEST_URL" ]; 
+then echo "TEST_URL null"; 
+else echo "TEST_URL $TEST_URL";
+fi;
+
+yarn run test:smoke

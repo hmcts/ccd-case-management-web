ad838c15d15fdea8db11b2db127609a76534fd89
development <dev@Ashleys-MacBook-Pro.local>
2018-10-02 16:08:19 +0100

    added new async cucumber functional tests

---
+++ test/functional-tests/stepDefinitions/baseSteps.js
@@ -0,0 +1,23 @@
+const {Before,After, Status , Given, When, Then} = require('cucumber');
+let fs = require('fs');
+
+    Before(async () => {
+        browser.ignoreSynchronization = true
+    });
+
+    After(async () => {
+      browser.close();
+    });
+
+    After(function (scenario, done) {
+        const world = this;
+        if (scenario.status === Status.FAILED) {
+                browser.takeScreenshot().then(stream => {
+                const decodedImage = new Buffer(stream.replace(/^data:image\/(png|gif|jpeg);base64,/, ''), 'base64');
+                world.attach(decodedImage, 'image/png');
+            })
+              .then(() => {
+                  done();
+              });
+        }
+    });

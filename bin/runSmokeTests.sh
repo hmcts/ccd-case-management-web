03db2f799cb1eef1090c83f0744b347d29c9ec39
Hemanth Potipati <hrpotipati@gmail.com>
2018-10-03 16:34:41 +0100

    Merge Pull Request #317
    * Add missing Vault secrets
    
    * Add missing secrets to Jenkins Nightly pipeline
    
    * Output Values on failure to load from vault
    
    * Update package.json
    
    * remove redundant docker usage for smoke tests
    
    * Neatify code
    
    * Trigger Preview env

--- bin/runSmokeTests.sh
+++ bin/runSmokeTests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-set -ex
 
-docker-compose -f docker-compose.tests.yml run --name "ccd-case-management-protractor" --rm -u `id -u $USER` protractor test:smokeTests
+ [[ -z $CCD_CASEWORKER_AUTOTEST_EMAIL ]] && echo "CCD_CASEWORKER_AUTOTEST_EMAIL is not set";
+ [[ -z $CCD_CASEWORKER_AUTOTEST_PASSWORD ]] && echo "CCD_CASEWORKER_AUTOTEST_PASSWORD is not set";
+ [[ -z $TEST_URL ]] && echo "TEST_URL is not set" || echo "TEST_URL=$TEST_URL";
+ 
+ yarn run test:smokeTest

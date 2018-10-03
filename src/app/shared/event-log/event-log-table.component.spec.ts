f78757782bb02245486584b917af7cfb6f99fccd
Karamath <karamath.mohamed@hmcts.net>
2018-10-03 11:16:33 +0100

    RDM-2901: Updated event model to include significant item

--- src/app/shared/event-log/event-log-table.component.spec.ts
+++ src/app/shared/event-log/event-log-table.component.spec.ts
@@ -20,7 +20,12 @@ describe('EventLogTableComponent', () => {
       state_name: 'Case Updated',
       user_id: 0,
       user_last_name: 'smith',
-      user_first_name: 'justin'
+      user_first_name: 'justin',
+      significant_item: {
+        type: 'DOCUMENT',
+        description: 'First document description',
+        url: 'https://google.com'
+      }
     },
     {
       id: 4,
@@ -33,7 +38,12 @@ describe('EventLogTableComponent', () => {
       state_name: 'Case Updated',
       user_id: 0,
       user_last_name: 'chan',
-      user_first_name: 'phillip'
+      user_first_name: 'phillip',
+      significant_item: {
+        type: 'NON-DOCUMENT',
+        description: 'Second document description',
+        url: 'https://google.com'
+      }
     }
   ];
   const SELECTED_EVENT = EVENTS[0];
@@ -141,5 +151,21 @@ describe('EventLogTableComponent', () => {
     expect(links.length).toBe(2);
     expect(links[0].nativeElement.getAttribute('href')).toBe('/event/5/history');
     expect(links[1].nativeElement.getAttribute('href')).toBe('/event/4/history');
+  });
+
+  it('should display icon if significant item exist', () => {
+    expect(component.significantItemExist(EVENTS[0])).toBeTruthy();
+  });
+
+  it('should not display icon if significant item does not exist', () => {
+    expect(component.significantItemExist(EVENTS[1])).toBeFalsy();
+  });
+
+  it('should contain the significant item description', () => {
+    expect(component.getSignificantItemDesc(EVENTS[0])).toEqual('First document description');
+  });
+
+  it('should contain the significant item url', () => {
+    expect(component.getSignificantItemUrl(EVENTS[1])).toEqual('https://google.com');
   });
 });

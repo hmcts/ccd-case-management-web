ea85a789d684050c018b37e4b4887c14d54e6855
Hemanth Potipati <hrpotipati@gmail.com>
2018-10-03 14:20:24 +0100

    Output Values on failure to load from vault

--- bin/runSmokeTests.sh
+++ bin/runSmokeTests.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-set -ex
 
-docker-compose -f docker-compose.tests.yml run --name "ccd-case-management-protractor" --rm -u `id -u $USER` protractor test:smokeTests
+##set -ex
+##docker-compose -f docker-compose.tests.yml run --name "ccd-case-management-protractor" --rm -u `id -u $USER` protractor test:smokeTests
+ 
+ if [ -z "$CCD_CASEWORKER_AUTOTEST_EMAIL" ] ; 
+then echo "CCD_CASEWORKER_AUTOTEST_EMAIL null"; 
+fi;
+
+ if [ -z "$CCD_CASEWORKER_AUTOTEST_PASSWORD" ]; 
+then echo "CCD_CASEWORKER_AUTOTEST_PASSWORD null"; 
+fi;
+
+ if [ -z "$TEST_URL" ]; 
+then echo "TEST_URL null"; 
+else echo "TEST_URL $TEST_URL";
+fi;
+
+ yarn run test:smokeTest

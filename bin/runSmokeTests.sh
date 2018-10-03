e1c8d17d2d9562909b5bfe15f6b80f37f616f0f4
Hemanth Potipati <hrpotipati@gmail.com>
2018-10-03 15:26:16 +0100

    Neatify code

--- bin/runSmokeTests.sh
+++ bin/runSmokeTests.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
- if [ -z "$CCD_CASEWORKER_AUTOTEST_EMAIL" ] ; 
-then echo "CCD_CASEWORKER_AUTOTEST_EMAIL null"; 
-fi;
-
- if [ -z "$CCD_CASEWORKER_AUTOTEST_PASSWORD" ]; 
-then echo "CCD_CASEWORKER_AUTOTEST_PASSWORD null"; 
-fi;
-
- if [ -z "$TEST_URL" ]; 
-then echo "TEST_URL null"; 
-else echo "TEST_URL $TEST_URL";
-fi;
-
+ [[ -z $CCD_CASEWORKER_AUTOTEST_EMAIL ]] && echo "CCD_CASEWORKER_AUTOTEST_EMAIL is not set";
+ [[ -z $CCD_CASEWORKER_AUTOTEST_PASSWORD ]] && echo "CCD_CASEWORKER_AUTOTEST_PASSWORD is not set";
+ [[ -z $TEST_URL ]] && echo "TEST_URL is not set" || echo "TEST_URL=$TEST_URL";
+ 
  yarn run test:smokeTest

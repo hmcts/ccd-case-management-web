83b3f3e232f028c91ad3a4c07180ff3a42b36fb2
development <dev@Ashleys-MacBook-Pro.local>
2018-10-04 15:32:55 +0100

    minor debugging

--- test/functional-tests/pageObjects/loginPage.js
+++ test/functional-tests/pageObjects/loginPage.js
@@ -21,10 +21,11 @@ class LoginPage extends BasePage {
       //await browser.get('https://ccd-case-management-web-saat.service.core-compute-saat.internal/');
     console.log('opening browser and navigating to url...')
       await browser.get(process.env.TEST_URL || 'http://localhost:3451',30000);
-    console.log('waiting for url to be /login')
       let EC = protractor.ExpectedConditions;
       let currentURL = await browser.getCurrentUrl();
-      await browser.wait(EC.urlContains(selfUrlPath),10000).catch(err => console.log(I`Failed to load page, Expected URL fragment: ${expectedUrlRegex} | Actual URL: ${currentURL}`));
+    console.log(I`waiting for url to be /login | current url is ${currentURL}`)
+
+    await browser.wait(EC.urlContains(selfUrlPath),10000).catch(err => console.log(I`Failed to load page, Expected URL fragment: ${expectedUrlRegex} | Actual URL: ${currentURL}`));
       console.log('finsihed waiting for /login and moving on')
       return new LoginPage
   }

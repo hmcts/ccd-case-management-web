0fe01697747d9976fe5b68a9db1ea7406af704e6
andrewf <andrzej.folga@hmcts.net>
2018-10-05 12:48:40 +0100

    RDM-3023 Code cleanup.

--- src/app/app.config.guard.ts
+++ src/app/app.config.guard.ts
@@ -1,12 +1,11 @@
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 import { AppConfig } from './app.config';
 import { Injectable } from '@angular/core';
-import { AbstractAppConfig } from '@hmcts/ccd-case-ui-toolkit';
 
 @Injectable()
 export class AppConfigGuard implements CanActivate {
 
-  constructor(private appConfig: AppConfig, private abstractAppConfig: AbstractAppConfig) {}
+  constructor(private appConfig: AppConfig) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
     return this.appConfig.load()

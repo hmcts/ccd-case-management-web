a38229c8cb0801217cb52d0b69470ef9ec6c7b02
Karamath <karamath.mohamed@hmcts.net>
2019-01-11 11:42:10 +0000

    2806: Tab focus to set on page main content

--- src/app/cases/viewer/case-viewer.component.ts
+++ src/app/cases/viewer/case-viewer.component.ts
@@ -60,6 +60,9 @@ export class CaseViewerComponent implements OnInit, OnDestroy {
     this.subscription = this.postViewActivity().subscribe((_resolved) => {
       // console.log('Posted VIEW activity and result is: ' + JSON.stringify(resolved));
     });
+    if (document.getElementById('case-viewer-control-print')) {
+      document.getElementById('case-viewer-control-print').focus();
+    }
   }
 
   ngOnDestroy() {

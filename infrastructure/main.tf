7cfe2722806af62c710a19f82bc4049c20b267a7
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2019-03-01 11:36:08 +0000

    Remove Terraform remote state for 'core-compute'
    RDM-4223

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 locals {
-  env_ase_url = "${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
+  env_ase_url = "${var.env}.service.core-compute-${var.env}.internal"
 
   default_ccd_gateway_url = "https://ccd-api-gateway-web-${local.env_ase_url}"
   ccd_gateway_url = "${var.ccd_gateway_url != "" ? var.ccd_gateway_url : local.default_ccd_gateway_url}"

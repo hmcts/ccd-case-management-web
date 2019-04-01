5bb173e946d658a972f8a94135136c386750bc30
tzarmango <fatih.ozceylan@gmail.com>
2019-04-01 10:36:55 +0100

    RDM-4473 exclude from test coverage config for app-insights

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['PhantomJS'],
+    exclude: ["/**/*app-insights*.ts"],
     singleRun: true
   });
 };

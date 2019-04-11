c3b79c751ed5201d2323f55ca0a68ddbba65a019
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-04-11 16:09:01 +0100

    RDM-4534 initial work

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -11,7 +11,6 @@ import { RouterModule } from '@angular/router';
 import { ProfileResolver } from './profile/profile.resolver';
 import { PaginationService } from './pagination/pagination.service';
 import { AlertModule } from './alert/alert.module';
-import { WorkbasketInputFilterService } from '../workbasket/workbasket-input-filter.service';
 import { OAuth2Service } from './auth/oauth2.service';
 import { ActivityResolver } from './activity/activity.resolver';
 import { CookiesComponent } from '../footer-nav/cookies.component';
@@ -57,7 +56,6 @@ import { DeviceDetectorModule } from 'ngx-device-detector';
     AddressesService,
     AuthService,
     OAuth2Service,
-    WorkbasketInputFilterService,
     CcdBrowserSupportComponent,
   ],
   declarations: [
--- src/app/workbasket/workbasket.module.ts
+++ src/app/workbasket/workbasket.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { WorkbasketComponent } from './workbasket.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { WorkbasketFiltersComponent } from './filters/workbasket-filters.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { CaseUIToolkitModule } from '@hmcts/ccd-case-ui-toolkit';
+import { WorkbasketFiltersModule } from '@hmcts/ccd-case-ui-toolkit/dist/shared/components';
 
 @NgModule({
   imports: [
@@ -15,13 +15,10 @@ import { CaseUIToolkitModule } from '@hmcts/ccd-case-ui-toolkit';
     SharedModule,
     CaseUIToolkitModule,
     ReactiveFormsModule,
+    WorkbasketFiltersModule,
   ],
   declarations: [
-    WorkbasketComponent,
-    WorkbasketFiltersComponent
-  ],
-  exports: [
-    WorkbasketFiltersComponent
+    WorkbasketComponent
   ]
 })
 export class WorkbasketModule {}

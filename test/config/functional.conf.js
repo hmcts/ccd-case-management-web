9ee689ab6dab8cc2cf0ed3e015cab14d33ebebd0
Hemanth Potipati <hrpotipati@gmail.com>
2019-04-16 23:34:34 +0100

    Update functional.conf.js

--- test/config/functional.conf.js
+++ test/config/functional.conf.js
@@ -44,8 +44,8 @@ exports.config = {
   plugins: [{
     package: require.resolve('protractor-multiple-cucumber-html-reporter-plugin'),
     options:{
-      jsonOutputPath: './smoke-output/test/results/',
-      reportPath: './smoke-output/test/results/',
+      jsonOutputPath: './functional-output/test/results/',
+      reportPath: './functional-output/test/results/',
 
       automaticallyGenerateReport: true,
       removeExistingJsonReportFile: true,

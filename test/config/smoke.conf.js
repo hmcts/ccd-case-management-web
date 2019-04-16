88d7afe8073af7f2d50f75f723fda920ab6e598a
Hemanth Potipati <hrpotipati@gmail.com>
2019-04-17 00:24:13 +0100

    Update smoke.conf.js

--- test/config/smoke.conf.js
+++ test/config/smoke.conf.js
@@ -1,59 +1,96 @@
 exports.config = {
 
+  /*
+     - before execution set the following environment variables:
+                CCD_CASEWORKER_AUTOTEST_EMAIL
+                CCD_CASEWORKER_AUTOTEST_PASSWORD
+                TEST_FRONTEND_URL
+     - to stop parallel execution: comment out shardTestFiles and maxInstances
+     - to stop tests running headless: comment out moz:firefoxOptions section
+  */
+
   specs: [
     '../features/*.feature'
   ],
 
   exclude: [],
 
+  allScriptsTimeout: 110000,
+  restartBrowserBetweenTests: true,
+  untrackOutstandingTimeouts: true,
+
   directConnect: true,
 
-  baseURL: '',
-
-  framework: 'custom',
-  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  framework: "jasmine2",
 
   capabilities: {
-    'shardTestFiles': true,
-    'maxInstances': 5,
-    'acceptInsecureCerts': true,
-    'marionette': false,
-    'browserName': 'chrome',
-
-    chromeOptions: { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote '] },
-
-    'proxy': {
+     "shardTestFiles": true,
+     "acceptInsecureCerts": true,
+     "maxInstances": 1,
+     "browserName": "chrome",
+      'proxy': {
       'proxyType': 'manual',
       'httpProxy': 'proxyout.reform.hmcts.net:8080',
       'sslProxy': 'proxyout.reform.hmcts.net:8080',
       'noProxy': ''
-    },
+   },
+   
+//     //Headless chrome
+//     chromeOptions: {
+//      args: process.env.HEADLESS
+//        ? [‘--headless’, ‘--no-sandbox’, ‘--disable-dev-shm-usage’]
+//        : [],
+//      binary: process.env.HEADLESS ? puppeteer.executablePath() : undefined,
+//    }, 
+    
+    "chromeOptions": {
+         "args": [
+            "--headless",
+            "no-sandbox",
+            "--disable-web-security"
+         ]
+     }
   },
 
-  cucumberOpts: {
-    require: [
-      '../stepDefinitions/*.js'
-    ],
-    tags: false,
-    format: 'json:test/results/results.json',
-    profile: false,
-    'no-source': true,
-    plugin: 'json:test/cucumber.json'
+//  capabilities: {
+//
+//      'shardTestFiles': true,
+//      'maxInstances': 2,
+//      'acceptInsecureCerts': true,
+//      'marionette': false,
+//      'browserName': 'firefox'
+//      'moz:firefoxOptions': {
+//            args: [ "--headless" ]
+//                 }
+//  },
+
+  jasmineNodeOpts: {
+    showTiming: true,
+    showColors: true,
+    isVerbose: false,
+    includeStackTrace: false,
+    defaultTimeoutInterval: 400000
+
   },
-  
-   plugins: [{
+
+  useAllAngular2AppRoots: true,
+
+
+  plugins: [{
         package: "protractor-screenshoter-plugin",
-        screenshotPath: "./smoke-output/test/results/",
+        screenshotPath: "./smoke-output",
         screenshotOnExpect: "failure+success",
         screenshotOnSpec: "failure+success",
         withLogs: true,
         writeReportFreq: "asap",
         clearFoldersBeforeTest: true
-}],
+    }],
 
-  useAllAngular2AppRoots: true,
+      onPrepare: function() {
+          // returning the promise makes protractor wait for the reporter config before executing tests
+          return global.browser.getProcessedConfig().then(function(config) {
+              //it is ok to be empty
+          });
+      }
 
-  SELENIUM_PROMISE_MANAGER: false,
-
-  allScriptsTimeout: 60000
 };

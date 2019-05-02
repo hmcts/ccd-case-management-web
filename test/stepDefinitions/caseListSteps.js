fa780a8f2619e6e886d10bb655f36c08e45f6a9c
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-05-02 08:50:09 +0100

    Result List functional tests (#622)
    * RDM-4128 migrated domains to ui-toolkit
    
    * RDM-4129 migrated services
    
    * RDM-4129 fixed test references
    
    * RDM-4130 initial succesfull try
    
    * RDM-4131 changed in accordance with new contract
    
    * RDM-4132 added feature file
    
    * RDM-4132 WIP
    
    * RDM-4132 done
    
    * RDM-2348-2: Grey bar styling
    
    * updated yarn lock file
    
    * updated yarn lock
    
    * updated toolkit version
    
    * Update package.json
    
    * marked broken tests
    
    * RDM-2348-2: Review comments
    
    * test fixes
    
    * updated tests
    
    * added search tests for case filters, changed locator of reset button
    
    * change reset locator
    
    * remove app code
    
    * remove caseFilters.feature as it is not part of this story
    
    * fix dropdown error in code
    
    * tweaked scenario

--- test/stepDefinitions/caseListSteps.js
+++ test/stepDefinitions/caseListSteps.js
@@ -1,4 +1,6 @@
 let CaseListPage = require('../pageObjects/caseListPage.js');
+let TestData = require('../utils/TestData.js');
+CaseFilters = require('../pageObjects/ccd-components/caseFilters.js');
 
 let chai = require("chai").use(require("chai-as-promised"));
 let expect = chai.expect;
@@ -12,6 +14,11 @@ defineSupportCode(function ({ Given, When, Then}) {
 
   Then(/^the case reference is displayed in the case list results with hyphens$/, async function () {
     await caseListPage.getNavBarComponent().clickCaseListLink();
+
+    let wbFilters = new CaseFilters();
+    await wbFilters.selectJurisdiction(TestData.jurisdiction);
+    await wbFilters.selectCaseType(TestData.caseType);
+    await wbFilters.clickApplyButton();
 
     let columnResults = await caseListPage.getCaseListComponent().getColumnResults('Case Reference');
     expect(await columnResults[0].getText()).to.match(/\d{4}-\d{4}-\d{4}-\d{4}/)

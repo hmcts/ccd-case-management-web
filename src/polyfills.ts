c52617dddb75182c2b6b2ba6199b2be7aee6ab5b
Rafal Kalita <rkalita@equalexperts.com>
2019-07-23 13:32:54 +0200

    RDM-5302 Updated class-transformer, core-js and polyfills.ts (#750)
    * RDM-5302 Updated class-transformer, core-js and polyfills.ts
    
    * RDM-5302 Remove back the imports in polyfills.ts and leave es6/reflect
    
    * updated with pre-release version
    
    * RDM-5302 uncomment more imports in polyfills.ts
    
    * RDM-5302 change imports in polyfills.ts
    
    * RDM-5302 change imports in polyfills.ts and revert ui-toolkit
    
    * RDM-5302 change imports in polyfills.ts
    
    * updated correct toolkit version
    
    * RDM-5302 revert change imports in polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -21,10 +21,10 @@
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
 import 'core-js/es6/symbol';
 import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
 // import 'core-js/es6/math';
 import 'core-js/es6/string';
 // import 'core-js/es6/date';

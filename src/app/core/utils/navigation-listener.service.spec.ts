2b77dbb0cc1fcfae8bfbd7723c6acc2ece6ed8d4
Mohammed Anas Khatri <mohammed.khatri@hmcts.net>
2019-07-30 15:38:18 +0100

    Added tests for navigation listen service

--- src/app/core/utils/navigation-listener.service.spec.ts
+++ src/app/core/utils/navigation-listener.service.spec.ts
@@ -1,20 +1,19 @@
 import {NavigationListenerService} from "./navigation-listener.service";
-import {AlertService, NavigationNotifier, NavigationOrigin} from "@hmcts/ccd-case-ui-toolkit";
+import {AlertService, NavigationNotifierService, NavigationOrigin} from "@hmcts/ccd-case-ui-toolkit";
 import {Router} from "@angular/router";
 import {Observable} from "rxjs";
 
-fdescribe('NavigationListenerService', () => {
+describe('NavigationListenerService', () => {
 
   let navigationListenerService:  NavigationListenerService;
   let alertService: AlertService;
-  let navigationNotifier: NavigationNotifier;
+  let navigationNotifier: NavigationNotifierService;
   let router: Router;
 
   beforeEach(() => {
-    alertService = jasmine.createSpyObj<AlertService>('alertService', ['success']);
+    alertService = jasmine.createSpyObj<AlertService>('alertService', ['setPreserveAlerts','success']);
     router = jasmine.createSpyObj<Router>('router', ['navigate']);
-    navigationNotifier = new NavigationNotifier();
-    activityService.verifyUserIsAuthorized.and.returnValue(true);
+    navigationNotifier = new NavigationNotifierService();
     navigationListenerService = new NavigationListenerService(alertService, navigationNotifier,router);
   });
 
@@ -22,7 +21,43 @@ fdescribe('NavigationListenerService', () => {
     let DRAFT_DELETED = NavigationOrigin.DRAFT_DELETED;
     navigationNotifier.announceNavigation({ action: DRAFT_DELETED})
     navigationListenerService.init();
-
+    expect(router.navigate).toHaveBeenCalledWith(['list/case']);
   });
+
+  it('test ERROR_DELETING_DRAFT navigation subscription', () => {
+    let ERROR_DELETING_DRAFT = NavigationOrigin.ERROR_DELETING_DRAFT;
+    navigationNotifier.announceNavigation({ action: ERROR_DELETING_DRAFT})
+    navigationListenerService.init();
+    expect(router.navigate).toHaveBeenCalledWith(['list/case']);
+  });
+
+  it('test NO_READ_ACCESS_REDIRECTION navigation subscription', () => {
+    let NO_READ_ACCESS_REDIRECTION = NavigationOrigin.NO_READ_ACCESS_REDIRECTION;
+    navigationNotifier.announceNavigation({ action: NO_READ_ACCESS_REDIRECTION})
+    navigationListenerService.init();
+    expect(router.navigate).toHaveBeenCalledWith(['/list/case']);
+  });
+
+  it('test DRAFT_RESUMED navigation subscription', () => {
+
+    let DRAFT_RESUMED = NavigationOrigin.DRAFT_RESUMED;
+    let navParam = { action: DRAFT_RESUMED, jid: 'j001',ctid: 'c001',etid: 'e001',theQueryParams:'id=1001'};
+    navigationNotifier.announceNavigation(navParam);
+    navigationListenerService.init();
+    expect(router.navigate).toHaveBeenCalledWith(  ['create/case',
+      navParam.jid,
+      navParam.ctid,
+      navParam.etid],{ queryParams: navParam.theQueryParams });
+  });
+
+  it('test EVENT_TRIGGERED navigation subscription', () => {
+    let EVENT_TRIGGERED = NavigationOrigin.EVENT_TRIGGERED;
+    let navParam = { action: EVENT_TRIGGERED, relativeTo: 'relativeTo',etid: 'e001',theQueryParams:'id=1001'};
+    navigationNotifier.announceNavigation(navParam);
+    navigationListenerService.init();
+    expect(router.navigate).toHaveBeenCalledWith(  ['trigger',
+      navParam.etid],{ queryParams: navParam.theQueryParams, relativeTo: navParam.relativeTo });
+  });
+
 
 });

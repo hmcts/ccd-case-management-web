c867cf55967a80e5539b70ec757e391b67831305
Hemanth Potipati <hrpotipati@gmail.com>
2019-12-10 15:27:00 +0000

    Revert "RDM-6635 - pay bulkscan integration (#836)"
    This reverts commit 6b3a5282e05ab27507992a40d2560e5ceeaadc3d.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -121,10 +121,6 @@ export class AppConfig extends AbstractAppConfig {
     return this.config.payments_url;
   }
 
-  public getPayBulkScanBaseUrl() {
-    return this.config.pay_bulk_scan_url;
-  }
-
   public getChromeMinRequiredVersion() {
     return this.config.chrome_min_required_version;
   }

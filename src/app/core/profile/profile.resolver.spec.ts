1b2d355fd63c89bba37ac58692cb646cd849be08
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-01-21 23:39:51 +0000

    RDM-7134: Optional Redirection of Users with Multiple Roles to Expert UI

--- src/app/core/profile/profile.resolver.spec.ts
+++ src/app/core/profile/profile.resolver.spec.ts
@@ -6,12 +6,12 @@ import { inject, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 
 describe('ProfileResolver', () => {
+  const JURISDICTION: Jurisdiction = createSpyObj<any>('jurisdiction', ['toString']);
+  const JURISDICTION_CONFIG: JurisdictionUIConfig = createSpyObj<any>('jurisdictionConfig', ['toString']);
+
   describe('resolve()', () => {
 
     const PROFILE: Profile = createSpyObj<any>('profile', ['toString']);
-    const JURISDICTION: Jurisdiction = createSpyObj<any>('jurisdiction', ['toString']);
-    const JURISDICTION_CONFIG: JurisdictionUIConfig = createSpyObj<any>('jurisdictionConfig', ['toString']);
-
     const SHUTTER_URL = 'shutter';
 
     let profileService: any;
@@ -40,46 +40,6 @@ describe('ProfileResolver', () => {
         ]
       });
 
-      it('should resolve profile using profile service', inject([ProfileResolver],
-        (resolver: ProfileResolver) => {
-          profileService.get.and.returnValue(profileObs);
-          jurisdictionService.getJurisdictionUIConfigs.and.returnValue(jurisdictionConfigsObs);
-
-          resolver.resolve().subscribe((p) => {
-            expect(p).toBe(profile);
-          });
-          expect(profileService.get).toHaveBeenCalled();
-          expect(jurisdictionService.getJurisdictionUIConfigs).toHaveBeenCalled();
-        }));
-
-      it('should redirect to shutter page when all jurisdictions are shut', inject([ProfileResolver],
-        (resolver: ProfileResolver) => {
-          jurisdictionService.isShuttered.and.returnValue(true);
-          const configs = createJurisdictionUIConfigs([true, true, true]);
-
-          profileService.get.and.returnValue(profileObs);
-          jurisdictionService.getJurisdictionUIConfigs.and.returnValue(Observable.of(configs));
-
-          resolver.resolve().subscribe();
-
-          expect(jurisdictionService.isShuttered).toHaveBeenCalledWith(configs, 1);
-          expect(router.navigate).toHaveBeenCalledWith([SHUTTER_URL]);
-        }));
-
-      it('should not redirect to shutter page when at least one jurisdiction is open', inject([ProfileResolver],
-        (resolver: ProfileResolver) => {
-          jurisdictionService.isShuttered.and.returnValue(false);
-          const configs = createJurisdictionUIConfigs([true, false, true]);
-
-          profileService.get.and.returnValue(profileObs);
-          jurisdictionService.getJurisdictionUIConfigs.and.returnValue(Observable.of(configs));
-
-          resolver.resolve().subscribe();
-
-          expect(jurisdictionService.isShuttered).toHaveBeenCalledWith(configs, 1);
-          expect(router.navigate).not.toHaveBeenCalled();
-        }));
-
       function createProfile(jurisdictions: Jurisdiction[]): Profile {
         return {
           ...PROFILE,
@@ -88,15 +48,59 @@ describe('ProfileResolver', () => {
           isCourtAdmin: () => (true)
         };
       }
-
-      function createJurisdictions(refs: string[]): Jurisdiction[] {
-        return refs.map(ref => ({ ...JURISDICTION, id: ref }));
-      }
-
-      function createJurisdictionUIConfigs(shuttered: boolean[]): JurisdictionUIConfig[] {
-        return shuttered.map(shut => ({ ...JURISDICTION_CONFIG, shuttered: shut }));
-      }
-
     });
-  })
+
+    it('should create profile resolver', inject([ProfileResolver],
+      (resolver: ProfileResolver) => {
+        expect(resolver).toBeTruthy();
+      }));
+
+    it('should resolve profile using profile service', inject([ProfileResolver],
+      (resolver: ProfileResolver) => {
+        profileService.get.and.returnValue(profileObs);
+        jurisdictionService.getJurisdictionUIConfigs.and.returnValue(jurisdictionConfigsObs);
+
+        resolver.resolve().subscribe((p) => {
+          expect(p).toBe(profile);
+        });
+        expect(profileService.get).toHaveBeenCalled();
+        expect(jurisdictionService.getJurisdictionUIConfigs).toHaveBeenCalled();
+      }));
+
+    it('should redirect to shutter page when all jurisdictions are shut', inject([ProfileResolver],
+      (resolver: ProfileResolver) => {
+        jurisdictionService.isShuttered.and.returnValue(true);
+        const configs = createJurisdictionUIConfigs([true, true, true]);
+
+        profileService.get.and.returnValue(profileObs);
+        jurisdictionService.getJurisdictionUIConfigs.and.returnValue(Observable.of(configs));
+
+        resolver.resolve().subscribe();
+
+        expect(jurisdictionService.isShuttered).toHaveBeenCalledWith(configs, 1);
+        expect(router.navigate).toHaveBeenCalledWith([SHUTTER_URL]);
+      }));
+
+    it('should not redirect to shutter page when at least one jurisdiction is open', inject([ProfileResolver],
+      (resolver: ProfileResolver) => {
+        jurisdictionService.isShuttered.and.returnValue(false);
+        const configs = createJurisdictionUIConfigs([true, false, true]);
+
+        profileService.get.and.returnValue(profileObs);
+        jurisdictionService.getJurisdictionUIConfigs.and.returnValue(Observable.of(configs));
+
+        resolver.resolve().subscribe();
+
+        expect(jurisdictionService.isShuttered).toHaveBeenCalledWith(configs, 1);
+        expect(router.navigate).not.toHaveBeenCalled();
+      }));
+  });
+
+  function createJurisdictions(refs: string[]): Jurisdiction[] {
+    return refs.map(ref => ({ ...JURISDICTION, id: ref }));
+  }
+
+  function createJurisdictionUIConfigs(shuttered: boolean[]): JurisdictionUIConfig[] {
+    return shuttered.map(shut => ({ ...JURISDICTION_CONFIG, shuttered: shut }));
+  }
 });

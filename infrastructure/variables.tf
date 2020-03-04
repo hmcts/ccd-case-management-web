21f5db5ac736e8d82b939d8d7f8abf22a8f03a42
Dan Lysiak <daniel.lysiak@cgi.com>
2020-03-04 14:20:06 +0000

    Add tf variable

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -91,3 +91,8 @@ variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
   default = ""
 }
+
+variable "shutter_redirect_url" {
+  type = "string"
+  default = ""
+}

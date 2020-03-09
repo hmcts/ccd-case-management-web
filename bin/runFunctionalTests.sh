2111375e2f390dca3fb4abd2f234622cbecf439a
hemantt <hrpotipati@gmail.com>
2020-03-09 13:09:04 +0000

    Merge branch 'master' into release-cut-17.2
    # Conflicts:
    #	charts/ccd-case-management-web/values.yaml
    #	yarn.lock

--- bin/runFunctionalTests.sh
+++ bin/runFunctionalTests.sh
@@ -4,6 +4,8 @@
  [[ -z $CCD_CASEWORKER_AUTOTEST_FE_PASSWORD ]] && echo "CCD_CASEWORKER_AUTOTEST_FE_PASSWORD is not set";
  [[ -z $TEST_URL ]] && echo "TEST_URL is not set" || echo "TEST_URL=$TEST_URL";
 
+export PROTRACTOR_OUTPUT_DIR=functional-output
+
 if [[ ${TEST_URL} != *"preview-staging"* ]];then
         yarn run test:functionalTests
 else

a6e3f3381ff5d7f88a337f53772c86a7dd3d0e0c
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-04-06 19:41:07 +0100

    RDM-7491: Fix Management Web error on /config endpoint

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -5,15 +5,14 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RequestInterceptor } from './core/interceptor/requestinterceptor';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
-    ModuleMapLoaderModule,
-    HttpClientModule
+    ModuleMapLoaderModule
   ],
   providers: [
     {

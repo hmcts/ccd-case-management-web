2ef135d78f49a69d149d7cd39ce5976d1179a8d6
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-04-17 15:58:10 +0100

    RDM-7491: Fixed test case failures

--- src/app/app.config.spec.ts
+++ src/app/app.config.spec.ts
@@ -1,8 +1,6 @@
 import { AppConfig, Config } from './app.config';
 import { async, inject, TestBed } from '@angular/core/testing';
-import { HttpModule, Response, ResponseOptions, XHRBackend } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from  '@angular/common/http/testing';
 
 describe('AppConfig', () => {
 
@@ -40,6 +38,8 @@ describe('AppConfig', () => {
   const BANNER_URL = DATA_URL + '/internal/banners/';
   const JURISDICTION_UI_CONFIGS_URL = DATA_URL + '/internal/jurisdiction-ui-configs/';
 
+  let httpMock: HttpTestingController;
+
   const MOCK_CONFIG: Config = {
     login_url: LOGIN_URL,
     logout_url: LOGOUT_URL,
@@ -73,24 +73,19 @@ describe('AppConfig', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpModule, HttpClientModule],
+      imports: [HttpClientTestingModule],
       providers: [
-        { provide: XHRBackend, useClass: MockBackend },
         AppConfig
       ]
     });
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   describe('load()', () => {
 
     it('should load config from public/config.json',
-      async(inject([AppConfig, XHRBackend], (appConfig: AppConfig, mockBackend) => {
-        mockBackend.connections.subscribe((connection) => {
-          connection.mockRespond(new Response(new ResponseOptions({
-            body: JSON.stringify(MOCK_CONFIG)
-          })));
-        });
-
+      async(inject([AppConfig], (appConfig: AppConfig) => {
+      
         appConfig
           .load()
           .then(() => {
@@ -128,6 +123,9 @@ describe('AppConfig', () => {
             expect(appConfig.getBannersUrl()).toEqual(BANNER_URL);
             expect(appConfig.getJurisdictionUiConfigsUrl()).toEqual(JURISDICTION_UI_CONFIGS_URL);
           });
+          let configRequest = httpMock.expectOne('/config.json');
+          configRequest.flush(MOCK_CONFIG);
+          httpMock.verify();
       })));
   });
 });

2524e213440b82de3077635ea53df168029fcd5d
Rajaram <rajaram.nagarajan@solirius.com>
2021-01-19 16:36:51 +0000

    Code changes based on review comments

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,19 +1,3 @@
 provider "azurerm" {
   features {}
 }
-
-data "azurerm_key_vault" "ccd_shared_key_vault" {
-  name                = "${var.raw_product}-${var.env}"
-  resource_group_name = "${var.raw_product}-shared-${var.env}"
-}
-
-data "azurerm_key_vault_secret" "appinsights_instrumentationkey" {
-  name         = "AppInsightsInstrumentationKey"
-  key_vault_id = "${data.azurerm_key_vault.ccd_shared_key_vault.id}"
-}
-
-resource azurerm_key_vault_secret "appinsights_instrumentationkey_secret" {
-  name         = "AppInsightsInstrumentationKey"
-  value        = data.azurerm_key_vault_secret.appinsights_instrumentationkey.value
-  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
-}

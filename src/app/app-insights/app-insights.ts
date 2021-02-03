1aeca70204a3562337e5b49cd60a645f304679aa
Rajaram <rajaram.nagarajan@solirius.com>
2021-02-03 22:01:54 +0000

    RDM-4473: Build issues fixed

--- src/app/app-insights/app-insights.ts
+++ src/app/app-insights/app-insights.ts
@@ -1,12 +1,12 @@
-const config = require("config");
-const appInsights = require("applicationinsights");
+const config = require('config');
+const appInsights = require('applicationinsights');
 
-const enabled = config.get("appInsights.enabled");
+const enabled = config.get('appInsights.enabled');
 
 const enableAppInsights = () => {
   if (enabled) {
-    const appInsightsKey = config.get("secrets.ccd.AppInsightsInstrumentationKey");
-    const appInsightsRoleName = config.get("appInsights.roleName");
+    const appInsightsKey = config.get('secrets.ccd.AppInsightsInstrumentationKey');
+    const appInsightsRoleName = config.get('appInsights.roleName');
     appInsights.setup(appInsightsKey)
       .setAutoDependencyCorrelation(true)
       .setAutoCollectConsole(true, true);

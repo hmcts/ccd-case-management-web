203efa7a90ca9b06890802e1894d3c5f3dca3f91
Rajaram <rajaram.nagarajan@solirius.com>
2021-02-04 11:15:03 +0000

    RDM-4473: Build issues fixed

--- src/app/app-insights/app-insights.ts
+++ src/app/app-insights/app-insights.ts
@@ -1,17 +1,18 @@
+import { AppConfig } from '../app.config';
 const config = require('config');
 const appInsights = require('applicationinsights');
 
-const enabled = config.get('appInsights.enabled');
-
-const enableAppInsights = () => {
-  if (enabled) {
+const enableAppInsights = (appConfig: AppConfig) => {
+  if (appConfig.getAppInsightsEnabled().toLowerCase() === 'true') {
     const appInsightsKey = config.get('secrets.ccd.AppInsightsInstrumentationKey');
-    const appInsightsRoleName = config.get('appInsights.roleName');
+    const appInsightsRoleName = appConfig.getAppInsightsRoleName();
     appInsights.setup(appInsightsKey)
       .setAutoDependencyCorrelation(true)
       .setAutoCollectConsole(true, true);
     appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = appInsightsRoleName;
+    console.log('Starting appInsights...');
     appInsights.start();
+    console.log('Started appInsights...');
   }
 };
 
